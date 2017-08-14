a12f3e6bf63a7edc0eb3854e9b665b537b6d55b4
Kealan Corr <kealanjcorr@gmail.com>
2017-08-14 19:49:03 +0000

    Added users controller and show view

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -17,6 +17,7 @@ class ProfilesController < ApplicationController
       render action: :new
     end
   end
+  
   private
   def profile_params
     params.require(:profile).permit(:first_name, :last_name, :job_title, :phone_number, :contact_email, :description)
